471daa3148399830df88f507d10014f8d7d6e7d9
Obtusis <ns857744@gmail.com>
2024-09-08 20:25:39 -0700

    added updatable and drawable groups for objects

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        pygame.Surface.fill(screen, (0,0,0))
-        pygame.display.flip()
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Creating two groups to iterate through and adding Player class to each
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
+    # pygame dependencies for screen/frames and initializing Player
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+    dt = 0
+    
+    # Main game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen.fill("black")
+
+        for item in updatable:
+            item.update(dt)
+        for item in drawable:
+            item.draw(screen)
+        
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+    
 if __name__ == "__main__":
     main()
